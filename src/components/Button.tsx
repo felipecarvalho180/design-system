dda8c1f4b4f4eb39a60000057961949d9f72d559
Felipe Carvalho <felipecarvalho180@gmail.com>
2022-10-12 20:07:13 -0300

    feat: create interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
+      {...props}
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded text-black font-semibold text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white"
+        "py-3 px-4 bg-cyan-500 rounded text-black font-semibold text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white",
+        className
       )}
     >
       {children}
